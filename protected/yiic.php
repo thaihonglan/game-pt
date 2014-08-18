98889d6fcb63bed00e7ecadab90924ce450f9d13
joephankm@gmail.com <joephankm@gmail.com@0debbf68-f7d9-070e-19e1-e45154527f50>
2014-08-18 12:10:14 +0000

    Original framework

---
+++ protected/yiic.php
@@ -0,0 +1,9 @@
+<?php
+
+defined('YII_VERSION') or define('YII_VERSION', '1.1.15');
+
+// change the following paths if necessary
+$yiic=dirname(__FILE__).'/vendor/yii-'.YII_VERSION.'/yiic.php';
+$config=dirname(__FILE__).'/config/console.php';
+
+require_once($yiic);
