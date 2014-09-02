a0f408e849a73fdc7be45a2bc694320084621fab
joephankm@gmail.com <joephankm@gmail.com@0debbf68-f7d9-070e-19e1-e45154527f50>
2014-09-02 06:31:08 +0000

    upload database creation and other

--- trunk/protected/config/main.php
+++ trunk/protected/config/main.php
@@ -1,5 +1,10 @@
 <?php
 
+// define core config
+define('THEME', 'classic');
+define('THEME_URL', 'themes'.DS.THEME.DS);
+define('PLUGIN_URL', 'themes'.DS.'plugins'.DS);
+
 // uncomment the following to define a path alias
 // Yii::setPathOfAlias('local','path/to/local-folder');
 
@@ -7,7 +12,7 @@
 // CWebApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'My Web Application',
+	'name'=>'Game Portal',
 
 	// preloading 'log' component
 	'preload'=>array('log'),
@@ -18,26 +23,29 @@ return array(
 		'application.components.*',
 	),
 
+	// MODULE
 	'modules'=>array(
 		// uncomment the following to enable the Gii tool
-		/*
 		'gii'=>array(
 			'class'=>'system.gii.GiiModule',
-			'password'=>'Enter Your Password Here',
+			'password'=>false,
 			// If removed, Gii defaults to localhost only. Edit carefully to taste.
 			'ipFilters'=>array('127.0.0.1','::1'),
 		),
-		*/
+		'home'
 	),
 
-	// application components
+	// THEME
+	'theme' => THEME,
+
+	// APPLICATION COMPONENTS
 	'components'=>array(
 		'user'=>array(
 			// enable cookie-based authentication
 			'allowAutoLogin'=>true,
 		),
-		// uncomment the following to enable URLs in path-format
-		/*
+
+		// ROUTE settings
 		'urlManager'=>array(
 			'urlFormat'=>'path',
 			'rules'=>array(
@@ -46,20 +54,35 @@ return array(
 				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
 			),
 		),
-		*/
-		'db'=>array(
-			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
-		),
-		// uncomment the following to use a MySQL database
-		/*
-		'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=testdrive',
-			'emulatePrepare' => true,
-			'username' => 'root',
-			'password' => '',
-			'charset' => 'utf8',
-		),
-		*/
+
+		// CLIENT SCRIPTS settings
+// 		'clientScript' => array(
+// 			'packages' => array(
+// 				'jquery' => array(
+// 					'baseUrl' => PLUGIN_URL,
+// 					'js' => array('jquery-2.1.1.min.js'),
+// 				),
+// 				'jquery-ui' => array(
+// 					'baseUrl' => PLUGIN_URL.'jquery-ui',
+// 					'js' => array('js/jquery-ui-1.10.4.min.js'),
+// 					'css' => array('css/ui-lightness/jquery-ui-1.10.4.css'),
+// 				),
+// 			),
+// 		),
+
+		// DATABASE settings
+// 		'db' => array(
+// 			'class'            => 'DbConnection',
+// 			'connectionString' => 'mysql:host=127.0.0.1;dbname=kmremind',
+// 			'emulatePrepare'   => true,
+// 			'username'         => 'root',
+// 			'password'         => 'root',
+// 			'charset'          => 'utf8',
+// 			'enableProfiling' => true,
+// 			'enableParamLogging' => true,
+// 		),
+
+		// ERROR and LOG settings
 		'errorHandler'=>array(
 			// use 'site/error' action to display errors
 			'errorAction'=>'site/error',
