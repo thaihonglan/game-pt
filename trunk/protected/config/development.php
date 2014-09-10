6d9a269f73d3a8bb63069df3d9eb9b0e9e302c17
thaihonglan@gmail.com <thaihonglan@gmail.com@0debbf68-f7d9-070e-19e1-e45154527f50>
2014-09-10 13:07:43 +0000

    Chuyển Views xuống dưới

--- trunk/protected/config/development.php
+++ trunk/protected/config/development.php
@@ -5,5 +5,17 @@ $configures = CMap::mergeArray(
 
 	// development configures go here
 	array(
+		'components' => array(
+			'db' => array(
+				'class'            => 'CDbConnection',
+				'connectionString' => 'mysql:host=127.0.0.1;dbname=gamept',
+				'emulatePrepare'   => true,
+				'username'         => 'root',
+				'password'         => '',
+				'charset'          => 'utf8',
+				'enableProfiling' => true,
+				'enableParamLogging' => true,
+			),
+		),
 	)
 );
