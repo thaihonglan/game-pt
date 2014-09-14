cd13648dd872116f9a32551fd930bd04e6b5cce8
joephankm@gmail.com <joephankm@gmail.com@0debbf68-f7d9-070e-19e1-e45154527f50>
2014-09-14 18:13:57 +0000

    product detail

---
+++ protected/modules/home/controllers/GameController.php
@@ -0,0 +1,52 @@
+<?php
+
+class GameController extends Controller
+{
+	/**
+	 * @var string the default layout for the views. Defaults to '//layouts/column2', meaning
+	 * using two-column layout. See 'protected/views/layouts/column2.php'.
+	 */
+	public $layout='//layouts/home/main';
+
+	public function actionDetail($pid)
+	{
+		$productDetail = Product::model()->findByPk($pid);
+
+		$productResource = SourceFile::model()->findByPk($productDetail->source_file_id, array('order' => 'id DESC', 'limit' => 1));
+
+		$productScreenshots = Screenshot::model()->findAllByAttributes(array('product_id' => $productDetail->id));
+
+		$this->render('detail', array(
+			'productDetail' => $productDetail,
+			'productResource' => $productResource,
+			'productScreenshots' => $productScreenshots,
+		));
+	}
+
+	// Uncomment the following methods and override them if needed
+	/*
+	public function filters()
+	{
+		// return the filter configuration for this controller, e.g.:
+		return array(
+			'inlineFilterName',
+			array(
+				'class'=>'path.to.FilterClass',
+				'propertyName'=>'propertyValue',
+			),
+		);
+	}
+
+	public function actions()
+	{
+		// return external action classes, e.g.:
+		return array(
+			'action1'=>'path.to.ActionClass',
+			'action2'=>array(
+				'class'=>'path.to.AnotherActionClass',
+				'propertyName'=>'propertyValue',
+			),
+		);
+	}
+	*/
+}
