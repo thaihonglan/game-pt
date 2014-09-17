75fd98ce3e2f0127b8e1fb00d9a35babe5777722
thaihonglan@gmail.com <thaihonglan@gmail.com@0debbf68-f7d9-070e-19e1-e45154527f50>
2014-09-17 16:18:11 +0000

    Update giao diện trang Detail

--- themes/classic/views/home/game/detail.php
+++ themes/classic/views/home/game/detail.php
@@ -10,9 +10,25 @@
 		<a href="/home/home/">Home</a> &gt;
 		<a href="/home/game/detail?pid=<?php echo $productDetail->id; ?>"><?php echo $productDetail->name; ?></a>
 	</div>
-
+			
+				
 	<div class="detail-l">
 		<div class="detailContent">
+		<div align="center" style="margin-bottom: 20px;">
+			<object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" id="Myflash" width="690" applicationComplete="init()"
+				codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab">
+					<param name="movie" value=<?php echo Yii::app()->request->baseUrl . '/resource/' . $productResource->path ?> />
+					<param name="quality" value="high" />
+					<param name="bgcolor" value="#869ca7" />
+					<param name="allowScriptAccess" value="sameDomain" />
+
+					<embed src="<?php echo Yii::app()->request->baseUrl . '/resource/' . $productResource->path ?>" quality="high" bgcolor="#869ca7" width="690" height="520" name="Myflash" align="middle"
+						play="true" loop="false" quality="high" allowScriptAccess="sameDomain"
+						type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer">
+					</embed>
+			</object>
+		</div>
+		
 			<div class="title-stat">
 				<div class="pic">
 					<img src="<?php echo IMAGE_URL . $productDetail->avatar; ?>" alt="">
@@ -53,27 +69,13 @@
 				</div>
 			</div>
 
-			<object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" id="Myflash" width="800" applicationComplete="init()"
-				codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab">
-					<param name="movie" value=<?php echo Yii::app()->request->baseUrl . '/resource/' . $productResource->path ?> />
-					<param name="quality" value="high" />
-					<param name="bgcolor" value="#869ca7" />
-					<param name="allowScriptAccess" value="sameDomain" />
-
-					<embed src="<?php echo Yii::app()->request->baseUrl . '/resource/' . $productResource->path ?>" quality="high" bgcolor="#869ca7" width="640" height="500" name="Myflash" align="middle"
-						play="true" loop="false" quality="high" allowScriptAccess="sameDomain"
-						type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer">
-					</embed>
-				</object>
-
-
 
 			<!-- Introduction begin-->
 			<div class="aboutIntr">
 				<div class="aoubtTxt">
 					<h2>
 						<span><?php echo $productDetail->name; ?></span>
-						Description
+						Giới thiệu :
 					</h2>
 					<div class="conTxt">
 						<p>
@@ -107,9 +109,76 @@
 		</div>
 	</div>
 
-	<div class="gameRank gameBorder">
-		Hinh de day
+	<div class="gameRank">
+		<div class="title">
+			<span class="on">Game tương tự</span>
+		</div>
+		<ul>
+			<?php foreach ($similar as $index => $similar): ?>
+			<li>
+				<span class="listIco listIco-<?=($index + 1)?>"></span>
+				<div class="nameDown">
+					<a href="/home/game/detail?pid=<?=$similar->id?>"><?=$similar->name?></a>
+					<span><?=$similar->download?> lần tải</span>
+				</div>
+				<div class="detailTxt">
+					<div class="pic">
+						<a href="/home/game/detail?pid=<?=$similar->id?>" target="_blank" title="<?=$similar->name?>">
+							<img alt="<?=$similar->name?>" src="<?=IMAGE_URL.$similar->avatar?>"/>
+							<span class="cover"></span>
+						</a>
+					</div>
+					<div class="txt">
+						<h4>
+							<a href="/home/game/detail?pid=<?=$similar->id?>" target="_blank" title="<?=$similar->name?>"><?=$similar->name?></a>
+						</h4>
+					</div>
+					<p class="clear" style="position:relative">
+						<span class="game-good voteTarget" href="#" voteStyle="mode_three">Like：<i>866</i></span>
+						<span><?=$similar->download?> lần tải</span>
+						<span class="addAnimate">+1</span>
+					</p>
+				</div>
+			</li>
+			<?php endforeach; ?>
+		</ul>
 	</div>
+	
+	<div class="gameRank">
+		<div class="title">
+			<span class="on">Chơi nhiều nhất</span>
+		</div>
+		<ul>
+			<?php foreach ($monthlyTopProducts as $index => $monthlyTopProduct): ?>
+			<li>
+				<span class="listIco listIco-<?=($index + 1)?>"></span>
+				<div class="nameDown">
+					<a href="/home/game/detail?pid=<?=$monthlyTopProduct->id?>"><?=$monthlyTopProduct->name?></a>
+					<span><?=$monthlyTopProduct->download?> lần tải</span>
+				</div>
+				<div class="detailTxt">
+					<div class="pic">
+						<a href="/home/game/detail?pid=<?=$monthlyTopProduct->id?>" target="_blank" title="<?=$monthlyTopProduct->name?>">
+							<img alt="<?=$monthlyTopProduct->name?>" src="<?=IMAGE_URL.$monthlyTopProduct->avatar?>"/>
+							<span class="cover"></span>
+						</a>
+					</div>
+					<div class="txt">
+						<h4>
+							<a href="/home/game/detail?pid=<?=$monthlyTopProduct->id?>" target="_blank" title="<?=$monthlyTopProduct->name?>"><?=$monthlyTopProduct->name?></a>
+						</h4>
+					</div>
+					<p class="clear" style="position:relative">
+						<span class="game-good voteTarget" href="#" voteStyle="mode_three">Like：<i>866</i></span>
+						<span><?=$monthlyTopProduct->download?> lần tải</span>
+						<span class="addAnimate">+1</span>
+					</p>
+				</div>
+			</li>
+			<?php endforeach; ?>
+		</ul>
+	</div> <!-- Top game end -->
+
 </div>
 
 <div class="clear"></div>
