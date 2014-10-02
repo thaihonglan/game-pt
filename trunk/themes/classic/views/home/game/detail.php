d03d4734d48072c59fa36e71316b221fb6dec665
joephankm@gmail.com <joephankm@gmail.com@0debbf68-f7d9-070e-19e1-e45154527f50>
2014-10-02 18:58:41 +0000

    comment

--- trunk/themes/classic/views/home/game/detail.php
+++ trunk/themes/classic/views/home/game/detail.php
@@ -10,23 +10,22 @@
 		<a href="<?php echo $this->createUrl('game/detail', array('pid' => $productDetail->id)); ?>"><?php echo $productDetail->name; ?></a>
 	</div>
 
-
 	<div class="detail-l">
 		<div class="detailContent">
-		<div align="center" style="margin-bottom: 20px;">
-			<object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" id="Myflash" width="690" applicationComplete="init()"
-				codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab">
-					<param name="movie" value=<?php echo Yii::app()->request->baseUrl . '/resource/' . $productResource->path ?> />
-					<param name="quality" value="high" />
-					<param name="bgcolor" value="#869ca7" />
-					<param name="allowScriptAccess" value="sameDomain" />
+			<div align="center" style="margin-bottom: 20px;">
+				<object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" id="Myflash" width="690" applicationComplete="init()"
+					codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab">
+						<param name="movie" value=<?php echo Yii::app()->request->baseUrl . '/resource/' . $productResource->path ?> />
+						<param name="quality" value="high" />
+						<param name="bgcolor" value="#869ca7" />
+						<param name="allowScriptAccess" value="sameDomain" />
 
-					<embed src="<?php echo Yii::app()->request->baseUrl . '/resource/' . $productResource->path ?>" quality="high" bgcolor="#869ca7" width="690" height="520" name="Myflash" align="middle"
-						play="true" loop="false" quality="high" allowScriptAccess="sameDomain"
-						type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer">
-					</embed>
-			</object>
-		</div>
+						<embed src="<?php echo Yii::app()->request->baseUrl . '/resource/' . $productResource->path ?>" quality="high" bgcolor="#869ca7" width="690" height="520" name="Myflash" align="middle"
+							play="true" loop="false" quality="high" allowScriptAccess="sameDomain"
+							type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer">
+						</embed>
+				</object>
+			</div>
 
 			<div class="title-stat">
 				<div class="pic">
@@ -105,6 +104,53 @@
 			</div>
 			<!-- Game picture end -->
 
+		</div>
+	</div>
+
+	<div class="detail-l detailContent">
+		<div id="mainBox" class="comment" app_id="5208297" appver_id="200002320">
+			<div class="title">
+				<h2 class="comment-ico">发表您的看法</h2>
+			</div>
+			<div id="scoreArea" class="score" style="display: block;">
+				<h3>评个分吧！</h3>
+				<span id="txtScoreMsg" class="scoreMsg"></span>
+				<p>
+					<span class="val setHover" val="1">1分</span>
+					<span class="val setHover" val="2">2分</span>
+					<span class="val setHover" val="3">3分</span>
+					<span class="val setHover" val="4">4分</span>
+					<span class="val setHover on" val="5">5分</span>
+					<b><span id="txtAverage" class="num">5</span>分</b>
+				</p>
+			</div>
+			<div id="commentArea">
+				<div id="loginText"></div>
+				<div id="commentBox" style="display: block;">
+					<div class="userName clear">
+						<input type="text" class="ipt_nickname" value="PP用户" readonly="readonly" style="color: rgb(51, 51, 51);">
+						<span>请文明发言，PP有你更精彩！</span>
+					</div>
+					<textarea class="ipt_content"></textarea>
+					<div class="btnBottom">
+						<!--<input type="text" class="ipt_checkcode" value="验证码" maxlength="4" />
+						<div class="yzmPic"><img class="img_checkcode" title="看不清图片？点击更换" /></div>-->
+						<span class="txt_msg" style="display: none;">评论发表成功，谢谢您的参与！</span> <span class="btn_submit setHover">发表评论</span> </div>
+					</div>
+				</div>
+				<ul id="commentListArea">
+					<li>
+						<p class="nameDate">
+							<span class="name"><b>kietminh</b>　</span>
+							<span class="stars">评分：5.0</span>
+							<span class="date">2014-10-03 01:04:42</span>
+						</p>
+						<div class="replayBox" uplink="0" c_id="10169305"></div>
+						<p class="contTxt">真好玩！</p>
+					</li>
+				</ul>
+				<div class="page clearfix"><div class="pagearea" id="pages"></div>
+			</div>
 		</div>
 	</div>
 
